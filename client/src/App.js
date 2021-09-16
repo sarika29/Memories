ee6a32f2c5748b5c7760df163579a92743630bf7
sarika29 <sarika292820@gmail.com>
2021-09-16 09:10:07 +0530

    Made changes in all files

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core'
+
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from "./images/memories.png";
+
+const App=()=>{
+
+    return(
+        <Container maxWidth="lg">
+           <AppBar position="static" color="inherit">
+             <Typography variant="h2" align="center">Memories</Typography>
+             <img src={memories} alt="memories" height="60"/>
+
+           </AppBar>
+           <Grow in>
+              <Container>
+                 <Grid container justify="space-between" alignItems="stretch" spacing="3">
+                    <grid item xs={12} sm={7}>
+                    <Posts/>
+
+                    </grid>
+                    <grid item xs={12} sm={4}>
+                    <Form/>
+                    
+                    </grid>
+                 
+                 </Grid>
+              </Container>
+
+           </Grow>
+
+
+        </Container>
+    );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/Posts.js';
+
+
+
+
+const app=express();
+app.use('/posts',postRoutes);
+app.use(bodyParser.json({limit:"30mb", extended: true }));
+app.use(bodyParser.urlencoded({limit:"30mb", extended: true }));
+app.use(cors());
+
+
+const CONNECTION_URL='mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.gewam.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+
+const PORT=process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL
+    // {useNewUrlParser: true,
+    //     useUnifiedTopology:true,
+    //     useFindAndModify: false,
+    //     retryWrites:true
+
+    // }
+    )
+   .then(()=>app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`)))
+   .catch((error)=>console.log(error.message));
+
+   
+  //mongoose.set('useFindAndModify',false);
