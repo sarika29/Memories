10b5df1a22c781adf2248ec206ff12ae3ea079b2
sarika29 <sarika292820@gmail.com>
2021-09-28 07:32:24 +0530

    code updated for client and server

--- server/index.js
+++ server/index.js
@@ -9,10 +9,11 @@ import postRoutes from './routes/Posts.js';
 
 
 const app=express();
-app.use('/posts',postRoutes);
+
 app.use(bodyParser.json({limit:"30mb", extended: true }));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true }));
 app.use(cors());
+app.use('/posts',postRoutes);
 
 
 const CONNECTION_URL='mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.gewam.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,18 @@
-import express from 'express'
-import {getPosts,createPost} from '../controllers/posts.js'
+import express from 'express';
 
-const router=express.Router();
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+
+
+
+
 
 // http://localhost:5000/posts
-router.get('/',getPosts);
-router.post('/',createPost);
-export default router;
